293afefe449bfc0ffb47aca9c56b46064eec5d79
Ibnunadhif161 <147597291+Ibnunadhif161@users.noreply.github.com>
2023-11-15 19:52:26 +0700

    Update hurdle 1-4 code.py

--- Tugas Alpro 2/Reeborg's World/Hurdle/hurdle 1-4 code.py
+++ Tugas Alpro 2/Reeborg's World/Hurdle/hurdle 1-4 code.py
@@ -1,1 +1,12 @@
+"""
+[==============================]
+MADE BY KELOMPOK 9
+Find More at https://github.com
+Member :
+1. Regha Rahmadian Bintang  156
+2. M Ibnu Nadhif            161
+3. Refany Dwi Lestari       170
 
+===============================
+This code can be used at :
+https://reeborg.ca/reeborg.html
