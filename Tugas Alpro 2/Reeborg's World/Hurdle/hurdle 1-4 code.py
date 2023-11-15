177259971d8e878d69ecadfd674010d451e2b108
Regha01010010 <147596855+Regha01010010@users.noreply.github.com>
2023-11-15 19:52:59 +0700

    Update hurdle 1-4 code.py

--- Tugas Alpro 2/Reeborg's World/Hurdle/hurdle 1-4 code.py
+++ Tugas Alpro 2/Reeborg's World/Hurdle/hurdle 1-4 code.py
@@ -10,3 +10,16 @@ Member :
 ===============================
 This code can be used at :
 https://reeborg.ca/reeborg.html
+to complete Hurdle 1-4 
+===============================
+"""
+#membuat fungsi yang digunakan untuk membuat robot dapat menghadap kanan
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+#membuat loop yang berfungsi agar selama robot belum mencapai tujuan maka harus melakukan serangkaian gerakan hingga mencapai finish
+while not at_goal():
+    #jika depan kosong atau tidak ada halangan maka :
+    if front_is_clear():
